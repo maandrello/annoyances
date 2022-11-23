8f034837959e87988d4d390e57b0179a1b9be7b0
joana costa <joanartdacosta@gmail.com>
2022-11-23 10:55:58 +0000

    addcategoriesandbook

--- app/models/annoyance.rb
+++ app/models/annoyance.rb
@@ -1,4 +1,5 @@
 class Annoyance < ApplicationRecord
   belongs_to :user
   validates :name, presence: true, uniqueness: true
+  CATEGORIES = ["Animals", "Objects", "Sounds", "Smells", "People"]
 end
