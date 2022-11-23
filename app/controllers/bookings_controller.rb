97cf57e161394c46d4f783dc3370c4749be1e80d
joana costa <joanartdacosta@gmail.com>
2022-11-23 09:56:59 +0000

    addbookingform

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,28 @@
+class BookingsController < ApplicationController
+
+  def index
+    @bookings = Booking.all
+    # if @booking.save
+      # @booking.status == "accepted"
+    # end
+  end
+
+  def new
+   @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.permit(:starting_time, :ending_time)
+  end
+end
